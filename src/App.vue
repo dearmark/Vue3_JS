27fa24b122d58a91c80cffb4b8ba30ea3993e2d7
Mark Weng <treesea20@gmail.com>
2021-09-01 10:20:14 +0800

    动态模板创建成功

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<script>
+import { defineComponent } from 'vue/dist/vue.esm-bundler.js'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  data() {
+    return {
+      currentTab: 'Home',
+      tabs: ['Home', 'Posts', 'Archive']
+    }
+  },
+  computed: {
+    currentTabComponent() {
+      return 'tab-' + this.currentTab.toLowerCase()
+    }
+  }
+});
+</script>
+
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />-->
+  <div id="dynamic-component-demo" class="demo">
+    <button
+      v-for="tab in tabs"
+      :key="tab"
+      :class="['tab-button', { active: currentTab === tab }]"
+      @click="currentTab = tab"
+    >{{ tab }}</button>
+
+    <!-- Inactive components will be cached! -->
+    <keep-alive>
+    <component :is="currentTabComponent"></component>
+    </keep-alive>
+  </div>
+</template>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+.demo {
+  font-family: sans-serif;
+  border: 1px solid #eee;
+  border-radius: 2px;
+  padding: 20px 30px;
+  margin-top: 1em;
+  margin-bottom: 40px;
+  user-select: none;
+  overflow-x: auto;
+}
+
+.tab-button {
+  padding: 6px 10px;
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
+  border: 1px solid #ccc;
+  cursor: pointer;
+  background: #f0f0f0;
+  margin-bottom: -1px;
+  margin-right: -1px;
+}
+.tab-button:hover {
+  background: #e0e0e0;
+}
+.tab-button.active {
+  background: #e0e0e0;
+}
+.demo-tab {
+  border: 1px solid #ccc;
+  padding: 10px;
+}
+</style>
